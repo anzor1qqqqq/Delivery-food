7730c614fbbdd408d63eb1ccef9881f879525cfe
Anzor <anzor.shadzhe@gmail.com>
2022-12-08 21:45:48 +0300

    modified:   css/stylr.css 	new file:   img/food-band/margarita.jpg 	new file:   img/food-band/meet.jpg 	new file:   img/food-band/norwegian.jpg 	new file:   img/food-band/pepperoni.jpg 	new file:   img/food-band/preview.jpg 	new file:   img/food-band/seven-cheeses.jpg 	new file:   img/food-band/tom-yam.jpg 	new file:   img/gusi-lebedi/calf-sauce.jpg 	new file:   img/gusi-lebedi/chick.jpg 	new file:   img/gusi-lebedi/chicken-soup.jpg 	new file:   img/gusi-lebedi/dumplings.jpg 	new file:   img/gusi-lebedi/ear.jpg 	new file:   img/gusi-lebedi/pig-chop.jpg 	new file:   img/gusi-lebedi/plov.jpg 	new file:   img/gusi-lebedi/preview.jpg 	new file:   img/gusi-lebedi/squid.jpg 	new file:   img/gusi-lebedi/zander.jpg 	new file:   img/icon/home.svg 	new file:   img/icon/logo.svg 	new file:   img/icon/logout.svg 	new file:   img/icon/rating.svg 	new file:   img/icon/search.svg 	new file:   img/icon/shopping-cart-white.svg 	new file:   img/icon/shopping-cart.svg 	new file:   img/icon/user.svg 	new file:   img/palki-skalki/burrito.jpg 	new file:   img/palki-skalki/cheeseburger.jpg 	new file:   img/palki-skalki/combo.jpg 	new file:   img/palki-skalki/fusion.jpg 	new file:   img/palki-skalki/preview.jpg 	new file:   img/palki-skalki/rome.jpg 	new file:   img/palki-skalki/udon.jpg 	new file:   img/pizza-burger/pizza-caesar.jpg 	new file:   img/pizza-burger/pizza-chef.jpg 	new file:   img/pizza-burger/pizza-dacha.jpg 	new file:   img/pizza-burger/pizza-meat.jpg 	new file:   img/pizza-burger/pizza-pepperoni.jpg 	new file:   img/pizza-burger/pizza-village.jpg 	new file:   img/pizza-burger/preview.jpg 	new file:   img/pizza-plus/pizza-classic.jpg 	new file:   img/pizza-plus/pizza-girls.jpg 	new file:   img/pizza-plus/pizza-hawaiian.jpg 	new file:   img/pizza-plus/pizza-oleole.jpg 	new file:   img/pizza-plus/pizza-plus.jpg 	new file:   img/pizza-plus/pizza-vesuvius.jpg 	new file:   img/pizza-plus/preview.jpg 	new file:   img/promo/kebab.png 	new file:   img/promo/pizza.png 	new file:   img/promo/sushi.png 	new file:   img/promo/vegetables.png 	new file:   img/social/fb.svg 	new file:   img/social/instagram.svg 	new file:   img/social/vk.svg 	new file:   img/srars.svg 	new file:   img/tanuki/azhi.jpg 	new file:   img/tanuki/black.jpg 	new file:   img/tanuki/fresh.jpg 	new file:   img/tanuki/nisuaz.jpg 	new file:   img/tanuki/preview.jpg 	new file:   img/tanuki/smoke.jpg 	new file:   img/tanuki/tanuki.jpg 	modified:   index.html 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+let servHTTP = (url, callback) => {
+    let request = new XMLHttpRequest();
+    request.open ('GET', url);
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.status === 200 && request.readyState == 4) {
+            const objProd = JSON.parse(request.response);
+            callback(objProd);
+        };
+    });
+};
+
 let modalBasket = () => {
     const buttonBasketElem = document.querySelector('.button_basket');
     const modal = document.querySelector('.modal');
@@ -20,7 +33,7 @@ let modalAuth = () => {
     const closeAuthButton = document.querySelector('.close-auth');
     const logInFormElem = document.querySelector('#logInForm');
     const loginText = document.querySelector('#login');
-    const passwordText = document.querySelector('#password');
+    //const passwordText = document.querySelector('#password');
     const userName = document.querySelector('.user-name');
     const buttonOut = document.querySelector('.button-out');
 
@@ -32,8 +45,6 @@ let modalAuth = () => {
     let user = localStorage.getItem('FirstUser');
 
     let authorized = () => {
-        console.log('Авторизован');
-
         let logOut = () => {
             user = null;
             buttonSigin.style.display = 'flex';
@@ -55,8 +66,6 @@ let modalAuth = () => {
     };
 
     let noAuthorized = () => {  
-        console.log('Не авторизован');
-
         let logIn = (event) => {
             event.preventDefault(); 
 
@@ -88,6 +97,99 @@ let modalAuth = () => {
     checkAuth();
 };
 
+let createCardRest = (elem) => {
+    const cardsRest = document.querySelector('.cards_rest');
+    const cardRestMenuElem = document.querySelector('.card_rest_menu');
+    const promoElem = document.querySelector('.promo');
+    const logo = document.querySelector('.logo_img');
+
+    let card = '';
+
+    for (let i = 0; i < 6; i++) {
+        card = `
+        <a class="card animate__animated animate__fadeInUp wow" data-name="${elem[i].products}">
+                        <img src="${elem[i].image}" class="img_card">
+                        <div class="rest_about">
+                            <div class="rest_about_text">
+                                <div class="rest_name">
+                                    <h3 class="title_name_rest">${elem[i].name}</h3>
+                                    <span class="about_time_delivery">${elem[i].time_of_delivery} мин</span>
+                                </div>
+                                <div class="rest_rating">
+                                <img src="/img/srars.svg" alt="">
+                                    <span class="stars_rest">${elem[i].stars} </span>
+                                    <div class="category_and_price price">От ${elem[i].price} ₽</div>
+                                    <div class="category_and_price category">${elem[i].kitchen}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+    `;
+
+    cardsRest.insertAdjacentHTML('afterbegin', card);
+
+    
+    };
+
+    //let cardElem = document.querySelectorAll('.card');
+
+    let restMenu = (event) => {
+
+        let menu = '';
+
+        let createRestMenu = (elem) => {
+            console.log(elem);
+            for (let i = 0; i < 6; i++) {
+                menu += `
+                <div class="card">
+                        <img src="${elem[i].image}" alt="" class="img_card">
+                        <div class="rest_about">
+                            <div class="rest_about_text">
+                                <div class="rest_name title_food">
+                                    <h3 class="title_name_food">${elem[i].name}</h3>
+                                </div>
+                                <div class="rest_rating">
+                                    <div class="about_food">${elem[i].description}</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="price_button_food">
+                            <button class="button_food">В корзину</button>
+                            <div class="price_food">${elem[i].price} ₽</div>
+                        </div>
+                    </div>
+                `
+
+                cardRestMenuElem.innerHTML = menu;
+            };
+        };
+
+         const target = event.target;
+         const targetCard = target.closest('.card');
+
+         let arr = targetCard.dataset.name;
+
+         if (targetCard) {
+           cardsRest.style.display = 'none';
+           cardRestMenuElem.style.display = 'flex';   
+           promoElem.style.display = 'none';
+           servHTTP(`db/${arr}`, createRestMenu);
+         };
+
+         logo.addEventListener('click', () => {
+            cardsRest.style.display = '';
+            cardRestMenuElem.style.display = 'none';   
+            promoElem.style.display = '';
+        });
+    };
+
+    cardsRest.addEventListener('click', restMenu);
+};
+
+
+
 modalAuth();
 modalBasket();
 new WOW().init();
+
+servHTTP('db/partners.json', createCardRest);
